f25b2ec31bf49264d6eefbf583359ae93217ce38
xiaohao.li <xiaohao.li@chinaredstar.com>
2017-07-21 16:37:14 +0800

    精简代码

--- rabbitMQ-Spring-test/src/test/java/com/lixiaohao/rabbitMQ/TestQueue.java
+++ rabbitMQ-Spring-test/src/test/java/com/lixiaohao/rabbitMQ/TestQueue.java
@@ -14,21 +14,18 @@ import java.util.Map;
  * Created by xiaohao.li on 2017/7/13.
  */
 @RunWith(value = SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-        "classpath:appContext.xml"})
+@ContextConfiguration(locations = {"classpath:appContext.xml"})
 public class TestQueue {
 
     @Autowired
     private Producer producer;
-//    @Value("#{appConfig['mq.queue']}")
-    private String queueId = "test_mq";
 
     @Test
     public void send(){
         try {
             Map<String, Object> map = new HashMap<String, Object>();
             map.put("data", "hello rabbitmqfffffffffffffffffffffffff");
-            producer.sendQueue("test_exchange",  "test_queue_patt", map);
+            producer.sendQueue("test_exchange",  "test.rabbitmq.1", map);
         } catch (Exception e) {
             e.printStackTrace();
         }
