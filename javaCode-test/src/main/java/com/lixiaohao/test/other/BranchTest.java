3faabfc48f06a2fd6fd68897b77e007f94c7e22a
xiaohao.li <xiaohao.li@chinaredstar.com>
2017-11-09 11:07:26 +0800

    精简代码

---
+++ javaCode-test/src/main/java/com/lixiaohao/test/other/BranchTest.java
@@ -0,0 +1,12 @@
+package com.lixiaohao.test.other;
+
+/**
+ * @Author: xiaohao.li@chinaredstar.com
+ * @Description:
+ * @Date: Created in 11:04 2017/11/9
+ */
+public class BranchTest {
+    public static void main(String[] args) {
+        System.out.println("测试一下而已");
+    }
+}
